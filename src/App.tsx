a8d729e682a804045813d7e1eb76b7cc485323fb
Oleksandr <kozyrev.sania@gmail.com>
2026-05-25 16:41:29 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const handleClick = (clickType: string) => {
+    switch (clickType) {
+      case 'all':
+        return getAll().then(data => {
+          setGoods(data);
+        });
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+      case 'five':
+        return get5First().then(data => {
+          setGoods(data);
+        });
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+      case 'red':
+        return getRedGoods().then(data => {
+          setGoods(data);
+        });
+    }
 
-    <GoodsList goods={[]} />
-  </div>
-);
+    return;
+  };
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => handleClick('all')}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => handleClick('five')}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => handleClick('red')}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
